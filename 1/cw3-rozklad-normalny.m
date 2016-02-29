1114920a583c0e73fcfa8f7f40ab74a18010f15b
Paweł Abramowicz <pawelabrams@gmail.com>
2016-02-29 10:41:39 +0100

    Added subplots to Lista 1, cw3

--- 1/cw3-rozklad-normalny.m
+++ 1/cw3-rozklad-normalny.m
@@ -4,10 +4,13 @@
 normalny = randn(300,1)
 
 % histogram o 20 słupkach
+subplot(221)
 hist(normalny, 20)
 
 % histogram o 100 słupkach
+subplot(222)
 hist(normalny, 100)
 
 % wykres świecowy
+subplot(212)
 boxplot(normalny, 1)
